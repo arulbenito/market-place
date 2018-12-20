f6304c57ce4e503f1fdb688dd060867ea51db97e
Arul Benito Rathinam <arulbenito_rathinam@intuit.com>
2018-12-20 11:14:07 -0800

    clean up

--- packages/client/src/test/components/Account/Account.test.js
+++ packages/client/src/test/components/Account/Account.test.js
@@ -4,6 +4,13 @@ import Account from '../../../components/Account/Account';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  componentDidMount() {
+    const user = getCurrentUser();
+    const data = searchWiningProjects(user);
+    console.log(user)
+
+    this.setState({ projectList: data });
+  };
   ReactDOM.render(<Account />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
